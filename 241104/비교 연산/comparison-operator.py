7a9c796b26444b4fd0e4963352067c659ff74b17
sghtao <155219612+sghtao@users.noreply.github.com>
2024-11-04 16:40:37 +0900

    Title: 비교 연산, Time: 42ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 241104/비교 연산/comparison-operator.py
@@ -0,0 +1,11 @@
+arr = input().split()
+
+a = int(arr[0])
+b = int(arr[1])
+
+print(1) if a>=b else print(0)
+print(1) if a>b else print(0)
+print(1) if a<=b else print(0)
+print(1) if a<b else print(0)
+print(1) if a==b else print(0)
+print(1) if a!=b else print(0)
