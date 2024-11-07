cd0d0a43d25466ce541cdc88794ab62a19336a4a
sghtao <155219612+sghtao@users.noreply.github.com>
2024-11-07 23:19:11 +0900

    Title: a 부터 b 까지, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241107/a 부터 b 까지/a-to-b.py
@@ -0,0 +1,12 @@
+arr = input().split()
+
+a = int(arr[0])
+b = int(arr[1])
+print(a, end = " ")
+
+while a < b:
+    if a % 2 != 0:
+        a *= 2
+    else:
+        a += 3
+    print(a, end = " ")
