4abe0b229aadf279a6a5eceed2ea098bed4bb70b
sghtao <155219612+sghtao@users.noreply.github.com>
2024-11-07 22:56:29 +0900

    Title: 높은 수에서 낮은 수까지, Time: 48ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 241107/높은 수에서 낮은 수까지/from-high-to-low.py
@@ -0,0 +1,13 @@
+arr = input().split()
+
+a = int(arr[0])
+b = int(arr[1])
+
+if a > b:
+    for i in range(a, b-1, -1):
+        print(i, end = " ")
+elif a < b:
+    for i in range(b, a-1, -1):
+        print(i, end = " ")
+else:
+    print(a)
