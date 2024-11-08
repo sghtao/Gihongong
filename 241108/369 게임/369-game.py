36bf264393316aa6a52698d6555d8f02278ff813
sghtao <155219612+sghtao@users.noreply.github.com>
2024-11-08 14:44:52 +0900

    Title: 369 게임, Time: 45ms, Memory: 15MB, Status: Passed - CodeTree

--- 241108/369 게임/369-game.py
+++ 241108/369 게임/369-game.py
@@ -4,5 +4,7 @@ for i in range(1, n+1):
     ni = str(i)
     if ("3" in ni) or ("6" in ni) or ("9" in ni):
         print(0, end = " ")
+    elif i % 3 == 0:
+        print(0, end = " ")
     else:
         print(i, end = " ")
