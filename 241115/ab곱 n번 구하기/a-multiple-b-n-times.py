fe98022f675bd5f6846c79523a44f2dc2df6284d
sghtao <155219612+sghtao@users.noreply.github.com>
2024-11-15 22:33:36 +0900

    Title: ab곱 n번 구하기, Time: 49ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 241115/ab곱 n번 구하기/a-multiple-b-n-times.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+
+for _ in range(n):
+    arr = input().split()
+    a, b = int(arr[0]), int(arr[1])
+
+    prod = 1
+    for i in range(a, b+1):
+        prod *= i
+    print(prod)
