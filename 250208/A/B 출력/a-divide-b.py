3fa5bf6c094cd50a8a48c8d01d016859b571761b
sghtao <155219612+sghtao@users.noreply.github.com>
2025-02-08 23:27:29 +0900

    Title: A/B 출력, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250208/A/B 출력/a-divide-b.py
+++ 250208/A/B 출력/a-divide-b.py
@@ -8,8 +8,9 @@ d = a % b
 result = str(q)+"."
 
 for i in range(20):
-    r *= 10
-    d = r // b
-    result += str(d)
-    remainder %= b
+    d *= 10
+    n = d // b
+    result += str(n)
+    d %= b
 
+print(result)
