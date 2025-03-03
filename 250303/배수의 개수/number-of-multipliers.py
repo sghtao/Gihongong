9dfdb1b73202ad536cff01da081fdb60c2f4e7b7
sghtao <155219612+sghtao@users.noreply.github.com>
2025-03-03 14:25:43 +0900

    Title: 배수의 개수, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 250303/배수의 개수/number-of-multipliers.py
+++ 250303/배수의 개수/number-of-multipliers.py
@@ -1,7 +1,9 @@
 cnt = 0
+cnt1 = 0
 for i in range(10):
     n = int(input())
-    if n % 2 != 0:
+    if n % 3 == 0:
         cnt += 1
-
-print(cnt)
+    if n % 5 == 0:
+        cnt1 += 1
+print(cnt, cnt1)
