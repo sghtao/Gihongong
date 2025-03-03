a969136ba50cc697ffa42e9df827d764b4a0c6b8
sghtao <155219612+sghtao@users.noreply.github.com>
2025-03-03 15:13:26 +0900

    Title: 정수 사이의 배수, Time: 40ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250303/정수 사이의 배수/multiplication-between-numbers.py
@@ -0,0 +1,9 @@
+a, b = map(int, input().split())
+sum_val = 0
+cnt = 0
+for i in range(a, b+1):
+    if i % 5 == 0 or i % 7 == 0:
+        sum_val += i
+        cnt += 1
+print(sum_val, end = " ")
+print(f"{sum_val / cnt:.1f}")
